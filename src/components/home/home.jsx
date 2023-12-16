e41d2eaa10a5d40d17bd1ee7c4b8aa3ec3642ecf
evasrevird <45438031+evasrevird@users.noreply.github.com>
2023-12-16 14:44:01 +0700

    Initial commit

---
+++ src/components/home/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from "./banner.jsx";
+import Category from "./category.jsx";
+
+const Home = () => {
+    return (
+        <div>
+            <Banner/>
+            <Category/>
+        </div>
+    );
+};
+
+export default Home;
